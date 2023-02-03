739d682bd773af22048808a5698c617ab9899072
A.Amani <didi1364@gmail.com>
2023-02-03 14:47:53 +0330

    refactor: delete extra comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -199,15 +199,7 @@ static void MX_CAN_Init(void)
 {
 
   /* USER CODE BEGIN CAN_Init 0 */
-//	  GPIO_InitTypeDef GPIO_InitStruct = {0};
-//	  GPIO_InitStruct.Pin = GPIO_PIN_11;
-//	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;//GPIO_MODE_AF_PP
-//	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-//	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-//
-//	  GPIO_InitStruct.Pin = GPIO_PIN_12;
-//	  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;//
-//	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* USER CODE END CAN_Init 0 */
 
   /* USER CODE BEGIN CAN_Init 1 */
